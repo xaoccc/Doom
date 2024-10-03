76c41483bc2b8bc6ae8ca739f8f4eee5a85c9dc8
xaoccc <xaocccc@gmail.com>
2024-10-03 13:32:45 +0300

    added some contents (should be created dynamically in future)

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -10,31 +10,19 @@ export default function Events() {
                     </div>
                     <div className="row margin-bottom-small center-xs">
                         <div className="col-md">
-                            <img src="img/001-achievement.svg" className="icon " />
-                            <h3>Benefit 1</h3>
-                            <p>Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas volutpat, diam enim. </p>
+                            <img src="../../public/images/event-1.jpg" className="icon event-img" />
+                            <h3>Deicide</h3>
+                            <p>Support bands: Jungle Rot, Maltworm. Price: 70BGN</p>
                         </div>
                         <div className="col-md">
-                            <img src="img/002-pencil.svg" className="icon" />
-                            <h3>Benefit 2</h3>
-                            <p>Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas volutpat, diam enim. </p>
+                            <img src="../../public/images/event-2.jpg" className="icon event-img" />
+                            <h3>$uicideboy$</h3>
+                            <p>Support bands: 100 Kila. Price: 80BGN</p>
                         </div>
                         <div className="col-md">
-                            <img src="img/003-idea.svg" className="icon" />
-                            <h3>Benefit 3</h3>
-                            <p>Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas volutpat, diam enim. </p>
-                        </div>
-                    </div>
-                    <div className="row center-xs">
-                        <div className="col-md">
-                            <img src="img/004-shopping-bag.svg" className="icon" />
-                            <h3>Benefit 4</h3>
-                            <p>Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas volutpat, diam enim. </p>
-                        </div>
-                        <div className="col-md">
-                            <img src="img/005-fountain-pen.svg" className="icon" />
-                            <h3>Benefit 5</h3>
-                            <p>Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas volutpat, diam enim. </p>
+                            <img src="../../public/images/event-3.jpg" className="icon event-img" />
+                            <h3>Mayhem</h3>
+                            <p>Support bands: Bolg. Price: 100BGN</p>
                         </div>
                     </div>
                 </div>
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -9,17 +9,17 @@ export default function Services() {
                 </div>
                 <div className="row icon-row">
                     <div className="col-xs center-xs">
-                        <img src="img/001-achievement.svg" className="icon" /><br/>
+                        <img src="../../public/images/service-1.jpg" className="icon icon service-img" /><br/>
                         Music Events
-                        <p>Duis autem vel eum iriure dolor in hendrerit in vulput</p>
+                        <p>We offer the ultimate destination for fans of extreme music! Immerse yourself in a night of death metal, thrash metal, gangsta rap, hard bass, hard techno, and gabba and many more. With electrifying performances, a pulsating dance floor, and a bar serving devilishly crafted cocktails, experience a night where chaos reigns and the beats never stop. Join us if you dare!</p>
                     </div>
                     <div className="col-xs center-xs">
-                        <img src="img/002-pencil.svg" className="icon" /><br/>
-                        Betting Events
-                        <p>Duis autem vel eum iriure dolor in hendrerit in vulput</p>
+                        <img src="../../public/images/service-2.webp" className="icon service-img" /><br/>
+                        BetZone
+                        <p>Step into the thrilling world of sports betting at BetZone, where excitement meets opportunity! Our state-of-the-art venue offers an immersive experience for betting enthusiasts and sports lovers alike. With multiple large screens broadcasting live events, you won't miss a moment of the action!</p>
                     </div>
                     <div className="col-xs center-xs">
-                        <img src="img/003-idea.svg" className="icon" /><br/>
+                        <img src="../../public/images/service-3.webp" className="icon service-img" /><br/>
                         Strip Club
                         <p>A haven for those who crave the forbidden, our temple of indulgence is bathed in crimson light. Here, the occult meets sensuality, a world where gothic elegance intertwines with raw, untamed lust. Our performers are goddesses of the night, masters of the seductive arts who weave a spell of temptation that transcends the ordinary. Underneath the dim glow of candlelight, every whispered breath and every lingering touch draws you deeper into the realm of forbidden ecstasy.</p>
                     </div>
@@ -40,6 +40,13 @@ export default function Services() {
                         <p>This place is not for ordinary people. The entry is limited only for chosen ones.</p>
                     </div>
                 </div>
+                {
+                localStorage.getItem('admin') === 'true' && (
+                    <div className="adminPanel">
+                        <button className="addEvent">Add Service</button>
+                    </div>
+                )
+            }
 
             </div>
         </section>
