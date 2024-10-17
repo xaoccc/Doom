ab3c95d583830321e98fa5349d2add7c23b87afd
xaoccc <xaocccc@gmail.com>
2024-10-17 11:50:48 +0300

    added events app to backend

---
+++ backend/backend/events/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)    
+    imageUrl = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
