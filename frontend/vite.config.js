d9742d4f9d724eba02aac3d70bfe7f6e023b6461
xaoccc <xaocccc@gmail.com>
2024-11-07 12:31:57 +0200

    docker files created, frontend success, backend - db error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,  // Explicitly set the port to 5173
+    host: '0.0.0.0', // This allows the server to be accessed from outside the container
+  }
 })
