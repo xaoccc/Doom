bf0ae23ed2d984d04f40bf33a0cd2b0adb27ed5b
xaoccc <xaocccc@gmail.com>
2024-11-08 12:11:23 +0200

    created a sample database for the app and some small fixes

---
+++ test.sql
@@ -0,0 +1,6 @@
+
+
+
+
+INSERT INTO public.events_event VALUES (12, 'Many More', 'Join us for drinks and metal ', '2026-01-05', '19:00:00', 'Sofia', 'https://www.alcatraz.be/img/bands/suffocation.jpg', '2024-11-04 12:31:25.13867+02', '2024-11-04 12:31:25.13867+02', 30);
+
