b079a1d6f68a47e4b913ef29bb3ecbf7573c3d23
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-18 18:00:41 +0200

    another test

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,9 @@ import RegisterForm from "../components/RegisterForm";
 
 export default function Register() {
     // const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
+    const baseURL = import.meta.env.VITE_API_URL_PROD;
+    console.log(baseURL);
     return (
-      <RegisterForm route={`${import.meta.env.VITE_API_URL_PROD}/api/user/register/`} method="register" />
+      <RegisterForm route={`${baseURL}/api/user/register/`} method="register" />
     );
   }
