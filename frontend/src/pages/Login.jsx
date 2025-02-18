ce666fdf50825813d8d42b549ca59b596bc603d9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-18 17:44:36 +0200

    login host test

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,6 @@ import RegisterForm from "../components/RegisterForm";
 export default function Login() {
   // const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
   return (
-    <RegisterForm route={`${import.meta.env.VITE_API_URL_PROD}/api/user/login/`} method="login" />
+    <RegisterForm route={"https://doomclub2-fkhzh6hge7fkdvak.italynorth-01.azurewebsites.net/api/user/login/"} method="login" />
   );
 }
