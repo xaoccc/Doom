58f460bf4ed4e7c4b1c6d3250f7ec9b2b5a71d5f
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-18 18:18:14 +0200

    remove hardcode login baseurl

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,8 @@
 import RegisterForm from "../components/RegisterForm";
 
 export default function Register() {
-    // const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
-    const baseURL = import.meta.env.VITE_API_URL_PROD;
-    console.log(baseURL);
+    const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
+
     return (
       <RegisterForm route={`${baseURL}/api/user/register/`} method="register" />
     );
