b5a1c050410ca325c7d9f995a441fa0045130d2a
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-18 17:34:12 +0200

    use the correct host for login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import RegisterForm from "../components/RegisterForm";
 
 export default function Login() {
-  const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
+  // const baseURL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
   return (
-    <RegisterForm route={`${baseURL}/api/user/login/`} method="login" />
+    <RegisterForm route={`${import.meta.env.VITE_API_URL_PROD}/api/user/login/`} method="login" />
   );
 }
