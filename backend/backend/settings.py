9e812a08d58bc9d87d128f73bf2e9cb143814522
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-19 16:36:39 +0200

    another env test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -196,10 +196,13 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = 'xaocccc@gmail.com'
-EMAIL_HOST_PASSWORD = 'nfev infk qngx cabb'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# Django cannot take the email and password from the environment variables in azure!!!!!!!!!!
+# EMAIL_HOST_USER = 'xaocccc@gmail.com'
+# EMAIL_HOST_PASSWORD = 'nfev infk qngx cabb'
 EMAIL_USE_TLS = True
+
+TEST_VAR = os.getenv('TEST_VAR')
 
 
