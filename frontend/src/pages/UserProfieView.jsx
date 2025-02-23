c3606d79f3512743274ba461e8827a0364a72654
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-23 14:19:41 +0200

    test

--- frontend/src/pages/UserProfieView.jsx
+++ frontend/src/pages/UserProfieView.jsx
@@ -4,19 +4,25 @@ import { Link, useSearchParams } from 'react-router-dom';
 import api from '../api';
 
 export default function UserProfileView() {
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         getUserData();
     }, []);
 
     const getUserData = async () => {
-        api.get("/api/user/view/")
-        .then((response) => {
-            console.log(response.data);
-            setUser(response.data.filter(user => user.email === localStorage.getItem('email')));
-        })
-        .catch((error) => console.error(`Error: ${error}`));
+        try {
+            const response = await api.get("/api/user/view/");           
+            const currentUser = response.data.find(user => user.email === localStorage.getItem('email'));            
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                console.warn("User not found in API response.");
+                setUser(null);
+            }
+        } catch (error) {
+            console.error(`Error fetching user data: ${error}`);
+        }
     }
 
 
@@ -24,8 +30,14 @@ export default function UserProfileView() {
         <div>            
             <h1>User Profile</h1>
             <h2>Here you can see your user profile</h2>
-            <h4>Email address: {`${user[0].email}`}</h4>
-            <h4>{(user[0].age) ? `Age: ${user[0].age}` : null}</h4>
+            {user ? (
+                <>
+                    <h4>Email address: {user.email}</h4>
+                    {user.age && <h4>Age: {user.age}</h4>}
+                </>
+            ) : (
+                <p>No user data...</p>
+            )}
         </div>
     )
 }
